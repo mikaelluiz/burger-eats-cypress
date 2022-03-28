cd456c3488ee151d975d3297c75180bd0490fc06
Mikael Luz <mikaelluizf@outlook.com>
2022-03-27 23:12:33 -0300

    ajustando suite home para os padrões do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 
 describe('home page', () => {
     it('App must be online', () => {
-        cy.visit('https://buger-eats.vercel.app')
+        cy.visit('/')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
 })
